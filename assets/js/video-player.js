afc09c5546c0d5068109cc8f2549829a1e98b7b0
Alfredahoy <Alfred.dj.hoi@gmail.com>
2019-06-24 19:30:35 +1200

    0.1

--- assets/js/video-player.js
+++ assets/js/video-player.js
@@ -1,21 +1,4 @@
-var jqueryPlayer = new Vimeo.Player($('#video-container'));
-
-jqueryPlayer.play().then(function() {
-    jqueryPlayer.unload().then(function() {
-        }).catch(function(error) {
-    });
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'PasswordError':
-                break;
-            case 'PrivacyError':
-                break;
-            default:
-                break;
-        }
-    });
-
-jqueryPlayer.off('play');    
+var vid = document.getElementById("video");
 
 $("#video-container").click(function(event){
     $("#video-container").addClass("active");
@@ -25,75 +8,27 @@ $("#video-container").click(function(event){
     $("html").addClass("locked");
     $("#subtitle").addClass("active");
     $("#counter").addClass("active");
-    jqueryPlayer.play().then(function() {
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'PasswordError':
-                break;
-            case 'PrivacyError':
-                break;
-            default:
-                break;
-        }        
-    });
-    
-    jqueryPlayer.on('pause', function(data) {
-        $("#counter").html("( Press Play )");
-    });
-    
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    } 
 });
 
-jqueryPlayer.on('timeupdate', function(data) {
-    
-    jqueryPlayer.off('pause'); 
-    
-    t = 0;
-    jqueryPlayer.getDuration().then(function(duration) {
+video.onwaiting = function(event) {
+    $("#counter").html("( Buffering )");
+};
 
-    $("#counter").html("( " + Math.round(data["seconds"]) + "s / " + Math.round(duration) + "s )");
-        
-    $("#video-forward").click(function(event){
-        t = data["seconds"] + 5;
-        if (t >= (duration - 0.25)){
-            t = duration - 0.25;
-        }
-        jqueryPlayer.setCurrentTime(t).then(function(seconds) {
-            }).catch(function(error) {
-                switch (error.name) {
-                    case 'RangeError':
-                        break;
-                    default:
-                        break;
-                }
-            }); 
-    });
-    
-    $("#video-rewind").click(function(event){
-        t = data["seconds"] - 5;
-        if (t <= 0) {
-            t = 0;
-        }
-        jqueryPlayer.setCurrentTime(t).then(function(seconds){
-        }).catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                    break;
-                default:
-                    break;
-            }
-        }); 
-    });
-        
-    jqueryPlayer.on('bufferstart', function(data) {
-        $("#counter").html("( Buffering )");
-    });
-        
-    jqueryPlayer.on('ended', function(data) {
-        $("#counter").html("( 80s / 80s )");
-    });    
-        
-    }).catch(function(error) {
-    });
+video.ontimeupdate = function(event) {
+    $("#counter").html("( " + Math.round(video.currentTime) + "s / " + Math.round(video.duration) + "s )");
+};
+
+$("#video-forward").click(function(event){
+    video.currentTime = video.currentTime + 5;
+});
+
+$("#video-rewind").click(function(event){
+    video.currentTime = video.currentTime - 5;
 });
 
 $("#video-close").click(function(event){
@@ -104,17 +39,17 @@ $("#video-close").click(function(event){
     $("html").removeClass("locked");
     $("#subtitle").removeClass("active");
     $("#counter").removeClass("active");
-    
-    jqueryPlayer.unload().then(function() {
-    }).catch(function(error) {
-    });
-    
+    video.load();
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#video-container").offset().top - 50
     }, 0);
+    p = 0;
 });
 
 $(document).keydown(function(event){
+    
+    event = event || window.event;
+    
     if (event.key === 'Escape' && $("#video-container").hasClass("active")) {
         $("#video-container").removeClass("active");
         $("#video-control").removeClass("active");
@@ -123,8 +58,16 @@ $(document).keydown(function(event){
         $("html").removeClass("locked");
         $("#subtitle").removeClass("active");
         $("#counter").removeClass("active");
-        jqueryPlayer.unload().then(function() {
-        }).catch(function(error) {
-        }); 
-    }
+        video.load(); 
+    } else if (event.key === 'ArrowLeft' && $("#video-container").hasClass("active")) {
+        video.currentTime = video.currentTime - 5;
+    } else if (event.key === 'ArrowRight' && $("#video-container").hasClass("active")) {
+        video.currentTime = video.currentTime + 5;
+    } else if (event.key == " " && $("#video-container").hasClass("active")) {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }    
+  }
 });
